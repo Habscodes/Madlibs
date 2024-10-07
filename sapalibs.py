614adb70af13b6c09ad62d104fa793c7126563bd
Shittu Abdullah <euclidhablo99@yahoo.com>
2024-10-07 17:48:23 +0100

    Initial commit

---
+++ sapalibs.py
@@ -0,0 +1,9 @@
+Name = input("Your Name: ")
+Amount = input("How much you get sef: ")
+Verb = input("Verb: ")
+Adjective = input("Adjective: ")
+
+sapalib = f" {Name} checked their wallet and saw just {Amount} Naira left. They signed and said, Sapa no just {Verb} person , e don carry me go. A friend passed by and asked, How far? \
+{Name} replied, My guy. na {Adjective} Sapa dey deal with me."
+
+print(sapalib)
---
+++ sapalibs2.py
@@ -0,0 +1,8 @@
+def sapa_madlib(name, amount, verb, adjective):
+    return f"""**Title:**_Sapa Plava_
+{name} checked their wallet and saw just {amount} Naira left. They signed and said, Sapa no just {verb} person , e don carry me go."
+A friend passed by and asked, "How far?"
+{name} replied, "My guy. na {adjective} Sapa dey deal with me."
+"""
+
+print(sapa_madlib("Johnyy", "500","leave","wicked"))
